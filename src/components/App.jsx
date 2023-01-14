ba37b4f5ea6b63597eca75830be7ed4823d8b618
Vladyslav Polishchuk <105670228+vladpolischchuk@users.noreply.github.com>
2023-01-14 13:34:03 +0200

    adds component Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import Profile from "./Profile/Profile";
+
+import user from '../data/user.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };

4d541aaf412db5e16b979548637b10066ee9cb68
Vladyslav Polishchuk <105670228+vladpolischchuk@users.noreply.github.com>
2023-01-14 14:12:07 +0200

    adds component Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import Profile from "./Profile/Profile";
-
 import user from '../data/user.json';
+
+import Statistics from "./Statistics/Statistics";
+import data from '../data/data.json';
 
 export const App = () => {
   return (
@@ -21,6 +23,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import css from './Statistics.module.css';
+
+const Statistics = ({
+    title,
+    stats
+}) => {
+    return (
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
+
+            <ul className={css.stat__list}>
+                {stats.map(item =>
+                    <li
+                        key={item.id}
+                        style={{
+                            backgroundColor:
+                                '#' + (((1 << 24) * Math.random()) | 0).toString(16),
+                        }}
+                        className={css.item}
+                    >
+                        <span className={css.label}>{item.label}</span>
+                        <span className={css.percentage}> {item.percentage}%</span>
+                    </li>
+                )}
+            </ul>
+        </section>
+    );
+};
+
+export default Statistics;
